e51680462e3bdc4e537aa2995b1767810fcb57c0
ellesylvia19 <160628729+ellesylvia19@users.noreply.github.com>
2024-02-24 00:10:29 +0300

    Add files via upload

---
+++ user_input.py
@@ -0,0 +1,7 @@
+#prompt user
+name = input('Enter your full name: ')
+age = input('Enter age: ')
+location = input('Enter location: ')
+
+print(f"Hello {name}, you are {age} years old and live in {location}.")
+
